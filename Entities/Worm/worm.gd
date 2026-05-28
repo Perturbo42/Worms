5569f012041e3d90ca2e7b252949c9092ef4b304
Perturbo42 <calvin.kim6@gmail.com>
2026-05-28 10:31:49 +0900

    Worm moves better
    also fires missile slightly better

--- Entities/Worm/worm.gd
+++ Entities/Worm/worm.gd
@@ -1,9 +1,15 @@
 class_name Worm extends CharacterBody2D
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var timer: Timer = $Timer
 @onready var marker: Marker2D = $Marker2D
 
 const MISSILE = preload("uid://siacw3omdria")
-const SPEED = 300.0
+const GROUND_SPEED = 300.0
+const GROUND_ACCEL = 1200
+const GROUND_DECEL = 1400
+const AIR_SPEED = 150.0
+const AIR_ACCEL = 500
+const AIR_DECEL = 0
 const JUMP_VELOCITY = -400.0
 const GRAVITY = 400.0
 enum Weapons{
@@ -11,7 +17,8 @@ enum Weapons{
 	Grenade
 }
 
-var weapon_velocity := 600.0
+var dir: float = -1
+var weapon_velocity := 1500.0
 var is_active: bool = false
 var curr_weapon: Weapons = Weapons.Missile
 
@@ -20,12 +27,12 @@ func _input(event: InputEvent) -> void:
 		if !event.is_pressed(): return
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			weapon_velocity += 20
-			weapon_velocity = clamp(weapon_velocity, 100, 1500)
+			weapon_velocity = clamp(weapon_velocity, 100, 2000)
 		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			weapon_velocity -= 20
-			weapon_velocity = clamp(weapon_velocity, 100, 1500)
+			weapon_velocity = clamp(weapon_velocity, 100, 2000)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	marker.look_at(get_global_mouse_position())
 	if is_active:
 		if Input.is_action_just_pressed("shoot"):
@@ -37,26 +44,41 @@ func _process(delta: float) -> void:
 				missile.gravity = GRAVITY
 
 
+
 func _physics_process(delta: float) -> void:
 	if !is_active:
 		#apply gravity, apply knockback
 		return
 	
+	#Read Input
+	dir = Input.get_axis("left","right")
+	var jump_pressed = Input.is_action_just_pressed("jump")
 
+	#handle direction
+	if dir < -0.05:
+		sprite.flip_h = false
+		marker.position.x = -80
+	elif dir > 0.05:
+		sprite.flip_h = true
+		marker.position.x = 80
+	
+	#handle horizontal movement
+	var target_speed = GROUND_SPEED if is_on_floor() else AIR_SPEED
+	var accel = GROUND_ACCEL if is_on_floor() else AIR_ACCEL
+	var decel = GROUND_DECEL if is_on_floor() else AIR_DECEL
+	if dir != 0.0:
+		velocity.x = move_toward(velocity.x, dir * target_speed, accel * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, decel * delta)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if jump_pressed and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
